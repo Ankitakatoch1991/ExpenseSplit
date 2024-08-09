cc8febd8a6744adc2fcc675532e91d2caad61d1e
Ankita <ankita.katoch@quanata.com>
2024-08-09 15:18:40 -0700

    Initial Commit

---
+++ ExpenseSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseSplit
+//
+//  Created by Ankita Katoch on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
