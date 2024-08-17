467200ae84a1fbf132636b49247afd5b49ba6882
Ankita <ankitakatoch1991@gmail.com>
2024-08-16 18:20:02 -0700

    Initial Commit

---
+++ ExpenseSplit/ExpenseSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseSplitApp.swift
+//  ExpenseSplit
+//
+//  Created by Ankita Katoch on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
